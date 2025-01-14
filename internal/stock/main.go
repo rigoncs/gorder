066ce2555529f3c846a3def2172e8a0d3b062dc0
rigon <2412789408@qq.com>
2025-01-14 14:44:06 +0800

    fix bugs

--- internal/stock/main.go
+++ internal/stock/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"github.com/rigoncs/gorder/common/config"
 	"github.com/rigoncs/gorder/common/genproto/stockpb"
 	"github.com/rigoncs/gorder/common/server"
 	"github.com/rigoncs/gorder/stock/ports"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
+
+func init() {
+	if err := config.NewViperConfig(); err != nil {
+		logrus.Fatal(err)
+	}
+}
 
 func main() {
 	serviceName := viper.GetString("stock.service-name")
