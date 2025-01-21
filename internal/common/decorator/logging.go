6a9cee9d5a649a699d59b1439bb4786427528901
rigon <2412789408@qq.com>
2025-01-21 15:16:01 +0800

    可观测性-日志优化

--- internal/common/decorator/logging.go
+++ internal/common/decorator/logging.go
@@ -2,6 +2,7 @@ package decorator
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"strings"
@@ -13,9 +14,10 @@ type queryLoggingDecorator[C, R any] struct {
 }
 
 func (q queryLoggingDecorator[C, R]) Handle(ctx context.Context, cmd C) (result R, err error) {
+	body, _ := json.Marshal(cmd)
 	logger := q.logger.WithFields(logrus.Fields{
 		"query":      generateActionName(cmd),
-		"query_body": fmt.Sprintf("%#v", cmd),
+		"query_body": string(body),
 	})
 	logger.Debug("Executing query")
 	defer func() {
@@ -23,6 +25,28 @@ func (q queryLoggingDecorator[C, R]) Handle(ctx context.Context, cmd C) (result 
 			logger.Info("Query execute successfully")
 		} else {
 			logger.Error("Failed to execute query", err)
+		}
+	}()
+	return q.base.Handle(ctx, cmd)
+}
+
+type commandLoggingDecorator[C, R any] struct {
+	logger *logrus.Entry
+	base   CommandHandler[C, R]
+}
+
+func (q commandLoggingDecorator[C, R]) Handle(ctx context.Context, cmd C) (result R, err error) {
+	body, _ := json.Marshal(cmd)
+	logger := q.logger.WithFields(logrus.Fields{
+		"command":      generateActionName(cmd),
+		"command_body": string(body),
+	})
+	logger.Debug("Executing command")
+	defer func() {
+		if err == nil {
+			logger.Info("Command execute successfully")
+		} else {
+			logger.Error("Failed to execute command", err)
 		}
 	}()
 	return q.base.Handle(ctx, cmd)
--- internal/common/decorator/metrics.go
+++ internal/common/decorator/metrics.go
@@ -30,3 +30,23 @@ func (q queryMetricsDecorator[C, R]) Handle(ctx context.Context, cmd C) (result 
 	}()
 	return q.base.Handle(ctx, cmd)
 }
+
+type commandMetricsDecorator[C, R any] struct {
+	base   CommandHandler[C, R]
+	client MetricsClient
+}
+
+func (q commandMetricsDecorator[C, R]) Handle(ctx context.Context, cmd C) (result R, err error) {
+	start := time.Now()
+	actionName := strings.ToLower(generateActionName(cmd))
+	defer func() {
+		end := time.Since(start)
+		q.client.Inc(fmt.Sprintf("command.%s.duration", actionName), int(end.Seconds()))
+		if err == nil {
+			q.client.Inc(fmt.Sprintf("command.%s.success", actionName), 1)
+		} else {
+			q.client.Inc(fmt.Sprintf("commands.%s.failure", actionName), 1)
+		}
+	}()
+	return q.base.Handle(ctx, cmd)
+}
